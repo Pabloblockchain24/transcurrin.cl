25d80b211c1013ad73d2b01b54f7a0b03818a20b
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2023-11-13 00:04:56 -0300

    new changes

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const instance  = axios.create({
-    baseURL: "https://transcurrin-cl.vercel.app",
+    baseURL: "https://transcurrin-cl-api.vercel.app",
     withCredentials: true
 })
 

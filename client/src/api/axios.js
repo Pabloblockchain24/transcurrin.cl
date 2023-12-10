a9c4ac4e94d7ce307cb97f5057620c2c4db830fe
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2023-12-10 12:21:13 -0300

    fixing routes 2

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const instance  = axios.create({
-    baseURL: "https://transcurrin-cl-api.vercel.app/api",
+    baseURL: "https://server-transcurrin.vercel.app/api",
     withCredentials: true
 })
 

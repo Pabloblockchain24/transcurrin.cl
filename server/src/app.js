f95072171f1dbb3524a6dbc1f2ee95415924b47a
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2023-12-09 23:31:12 -0300

    trying resolver cors 4

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 //aqui configuramos el codigo backend (express)
 import  express  from "express";
 import cookieParser from "cookie-parser"
-import cors from "cors"
+// import cors from "cors"
 
 
 import authRoutes from "./routes/auth.routes.js"
@@ -9,27 +9,10 @@ import intranetRoutes from "./routes/intranet.routes.js"
 
 const app = express()
 
-const allowCors = async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-      res.status(200).end()
-      return
-    }
-    return await fn(req, res)
-  }
-
-allowCors()
-
-app.use(cors({
-    origin: "https://transcurrin-cl-client.vercel.app",
-    credentials:true
-}))
+// app.use(cors({
+//     origin: "https://transcurrin-cl-client.vercel.app",
+//     credentials:true
+// }))
 
 app.use(express.json())
 app.use(cookieParser())

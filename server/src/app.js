1e3bcf0d1872b3074a4cefe2cdfe7428df48e9fd
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2023-12-09 23:22:07 -0300

    fixing cors block 3

--- server/src/app.js
+++ server/src/app.js
@@ -9,8 +9,28 @@ import intranetRoutes from "./routes/intranet.routes.js"
 
 const app = express()
 
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+  }
 
+  
+  const handler = (req, res) => {
+    const d = new Date()
+    res.end(d.toString())
+  }
 
+allowCors(handler)
 
 app.use(cors({
     origin: "https://transcurrin-cl-client.vercel.app",

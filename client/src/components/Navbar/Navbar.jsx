5dd3d0c86fd45ca3c114568bb6ab0a2eb18b0331
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2023-12-17 23:20:37 -0300

    navbar responsive

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,47 +1,57 @@
 import React from 'react'
+import { useState, useEffect  } from 'react'
 import "./Navbar.css"
-import { NavLink, Link } from "react-router-dom"
+import { NavLink, Link, useLocation } from "react-router-dom"
 import { useAuth } from "../../context/AuthContext"
 import logoTrancurrin from "../../assets/logoSitio2.png"
 import { BsPersonGear } from "react-icons/bs";
+import { PiListFill } from "react-icons/pi";
+import { AiFillCloseSquare } from "react-icons/ai";
+
 
 function Navbar() {
     const { isAuthenticated, user, logout } = useAuth()
+    const [menuOpen, setMenuOpen] = useState(false);
+    const location = useLocation();
 
-    return (
-        <nav className="navbarMain">
-            <button className='abrirMenu'>ABRIR</button>
-                {isAuthenticated ?
-                    (
-                        <main className='navbarMainBox'>
-                            <section className='navbarLogoBox'>
-                                <NavLink to={"/intranet"}>
-                                    {<img src={logoTrancurrin} alt="Logo" className='navbarLogo' />}
-                                </NavLink>
-                            </section>
-                            <section className='navbarList'>
-                                <div className='navbarTitle'> BIENVENIDO {user.name} ! </div>
-                                <Link className="navbarLog" to="/" onClick={() => { logout() }}> Logout</Link>
-                            </section>
-                        </main>
-                    ) : (
-                        <main className='navbarMainBox'>
-                            <section className='navbarLogoBox'>
-                                <NavLink to={"/"}>
-                                    {<img src={logoTrancurrin} alt="Logo" className="navbarLogo" />}
-                                </NavLink>
-                            </section>
-                            <section className='navbarList'>
-                                <NavLink to={"/categoria/NOSOTROS"} className="navbarLink"> NOSOTROS </NavLink>
-                                <NavLink to={"/categoria/DEPOSITOEQUIPOS"} className="navbarLink"> EQUIPOS</NavLink>
-                                <NavLink to={"/categoria/SERVICIOS"} className="navbarLink"> SERVICIOS </NavLink>
-                                <NavLink to={"/categoria/CLIENTES"} className="navbarLink"> CLIENTES </NavLink>
-                                <NavLink to={"/categoria/CONTACTO"} className="navbarLink"> CONTACTO </NavLink>
-                                <NavLink to={"/login"} className="navbarLog"> <BsPersonGear className='navbarLogoLog' /> INTRANET </NavLink>
-                            </section>
-                        </main>
-                    )}
-        </nav>
-    )
+
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+      };
+      
+      useEffect(() => {
+        setMenuOpen(false);
+      }, [location.pathname]);
+
+      return (
+        <header className="navbarMain">
+            <NavLink to={isAuthenticated ? '/intranet' : '/'}>
+              <img src={logoTrancurrin} alt="Logo" className="navbarLogo" />
+            </NavLink>
+          <button className="menuMobile" onClick={toggleMenu}>
+            <PiListFill className='buttonAbrir' />
+          </button>
+          <nav className={menuOpen ? 'navbarCustom visible' : 'navbarCustom'}>
+            {isAuthenticated ? (
+              <ul className="navbarList">
+                <li> <div className="navbarTitle">BIENVENIDO {user.name}!</div> </li>
+                <Link className="navbarLog" to="/" onClick={() => logout()}> LOGOUT </Link>
+              </ul>
+            ) : (
+
+              <ul className="navbarList">
+                <button className="cerrarMenuMobile" onClick={toggleMenu}><AiFillCloseSquare /></button>
+                <li><NavLink to="/categoria/NOSOTROS" className="navbarLink"> NOSOTROS </NavLink> </li>
+                <li><NavLink to="/categoria/DEPOSITOEQUIPOS" className="navbarLink"> EQUIPOS</NavLink></li>
+                <li><NavLink to="/categoria/SERVICIOS" className="navbarLink">SERVICIOS</NavLink></li>
+                <li><NavLink to="/categoria/CLIENTES" className="navbarLink">CLIENTES</NavLink></li>
+                <li><NavLink to="/categoria/CONTACTO" className="navbarLink">CONTACTO</NavLink></li>
+                <NavLink to="/login" className="navbarLog"><BsPersonGear className="navbarLogoLog" /> INTRANET</NavLink>
+              </ul>
+            )}
+          </nav>
+        </header>
+      );
 }
 export default Navbar

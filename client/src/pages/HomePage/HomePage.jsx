52dbd4be5f6b8eae56c9a548948b0f6573a084ac
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2023-12-17 23:51:10 -0300

    homePage responsive

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -3,19 +3,13 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-
-
 import CountUp from 'react-countup';
 import transparencia from "../../assets/TRANSPARENCIA.jpg"
 import responsabilidad from "../../assets/RESPONSABILIDAD.jpg"
 import disponibilidad from "../../assets/DISPONIBIIDAD.jpg"
-
-
 import "./HomePage.css"
 
 function HomePage() {
-  
-  
   
   const settings = {
     dots: false,
@@ -28,64 +22,47 @@ function HomePage() {
     arrows:true,
   }
 
-
-
   const mapURL = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6648.896407065845!2d-71.56854969521515!3d-33.567717428254156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662475ce1f7c805%3A0x8e55b0b74dd1bcfa!2sFuenzalida%20Almacen%20de%20Contenedores%20Ltda!5e0!3m2!1ses!2scl!4v1702121617751!5m2!1ses!2scl";
-
-
-
 
   return (
     <>
 
     <Slider {...settings}>
-    <div className='customBanner'>
-      <div className='aux'>
-        <h1 className='customTitleBanner' >SOLUCIONES LOGISTICAS A TU MEDIDA</h1>
-        <h2 className='customTitleBanner2'> Nos adaptamos a tus necesidades </h2>
+    <div className='Banner1'>
+      <div className='BannerSection'>
+        <h1 className='BannerTitle1' >SOLUCIONES LOGISTICAS A TU MEDIDA</h1>
+        <h2 className='BannerTitle2'> Nos adaptamos a tus necesidades </h2>
         <button className='buttonBanner'> CONTACTANOS AQUI</button> 
       </div>
     </div>
 
-
-    <div className='customBanner2'>
-      <div className='aux'>
-        <h1 className='customTitleBanner' >EXPERIENCIA A TU SERVICIO</h1>
-        <h2 className='customTitleBanner2'> 35 años de operación nos respaldan </h2>
+    <div className='Banner2'>
+      <div className='BannerSection'>
+        <h1 className='BannerTitle1' >EXPERIENCIA A TU SERVICIO</h1>
+        <h2 className='BannerTitle2'> 35 años de operación nos respaldan </h2>
         <button className='buttonBanner'> CONTACTANOS AQUI</button> 
       </div>
     </div>
 
-    <div className='customBanner3'>
-      <div className='aux'>
-        <h1 className='customTitleBanner' >CENTRATE EN TU NEGOCIO PRINCIPAL</h1>
-        <h2 className='customTitleBanner2'> Despreocupate de la logistica de tu carga</h2>
+    <div className='Banner3'>
+      <div className='BannerSection'>
+        <h1 className='BannerTitle1' >CENTRATE EN TU NEGOCIO PRINCIPAL</h1>
+        <h2 className='BannerTitle2'> Despreocupate de la logistica de tu carga</h2>
         <button className='buttonBanner'> CONTACTANOS AQUI</button> 
       </div>
     </div>
-
 
   </Slider>
 
 
     <section className='sectionPadre'>
-
- 
-
-
-
-
-
-
       <hr className='footerLine1' />
-
 
       <section className='customSection1'>
 
         <h1 className='customTitleSection1'>2022 EN NÚMEROS</h1>
 
         <section className='contadores'>
-
 
           <article className='articleContadores'>
             <CountUp start={0} end={1500} duration={10} separator="," className='itemCount' formattingFn={(value) => `+${value}`} />

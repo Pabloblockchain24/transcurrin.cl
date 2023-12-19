9f541cdbcd8d95177497208aba8296ad3f34beae
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2023-12-18 21:12:30 -0300

    footer responsive done

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,47 +5,47 @@ import logoFooter from "../../assets/logoFooter.png"
 
 function Footer() {
   return (
-    <div className="flex flex-col">
-      <section className="footerTop">
-        <footer className="flex flex-col items-center">
-          <img
-            src={logoFooter}
-            alt="logo tienda"
-            className="w-3/5"
-          />
-        </footer>
-        <section className="footer">
-          <article className="customArticle">
-            <p className="customPTitle">CONTACTO</p>
-            <p className="customP"> +569 5072 7028</p>
-          </article>
-          <article className="customArticle">
-            <p className="customPTitle">DEPOSITO Y OPERACIONES</p>
-            <p className="customP">LAS ACACIAS 315 AGUAS BUENAS , SAN ANTONIO</p>
-          </article>
-          <article className="customArticle">
-            <p className="customPTitle">OFICINA COMERCIAL </p>
-            <p className="customP">BLANCO ENCALADA #750, SAN ANTONIO</p>
-          </article>
-          <hr className="footerLine" />
-          <div className="footerBottom">
-            <p>©transcurrin.cl All rigths reserved</p>
-          </div>
+    <footer className="FooterMain">
+        <section className="FooterLogo">
+          <img src={logoFooter} alt="Logo Transcurrin"/>
         </section>
-        <section className="section footer">
-          <article className="customArticle2">
-            <p className="customPTitle">MAIL CONTACTO</p>
-            <p className="customP"> parce@transcurrin.cl</p>
-            <p className="customP"> contacto@transcurrin.cl</p>
-          </article>
-          <article className="customCotizacion">
-            <NavLink to={"/categoria/CONTACTO"} className="customButtonCotizacion"> OBTEN TU COTIZACIÓN HOY </NavLink>
 
-            <p className="customP"> ¡ Solo válido para días hábiles !</p>
+
+        <section className="FooterCentro">
+          <article className="FooterArticle">
+            <h2 className="FooterTitle">CONTACTO</h2>
+            <p className="FooterDescription"> +569 5072 7028</p>
           </article>
+
+
+          <article className="FooterArticle">
+            <h2 className="FooterTitle">DEPOSITO Y OPERACIONES</h2>
+            <p className="FooterDescription">LAS ACACIAS 315 AGUAS BUENAS , SAN ANTONIO</p>
+          </article>
+          <article className="FooterArticle">
+            <h2 className="FooterTitle">OFICINA COMERCIAL </h2>
+            <p className="FooterDescription">BLANCO ENCALADA #750, SAN ANTONIO</p>
+          </article>
+
+          <hr className="FooterLine" />
+            <div className="FooterCentroBottom">©transcurrin.cl All rigths reserved</div>
         </section>
-      </section>
-    </div>
+
+        <section className="FooterEnd">
+          <article className="FooterEndContact">
+            <h2 className="FooterTitle">MAIL CONTACTO</h2>
+            <p className="FooterDescription"> parce@transcurrin.cl</p>
+            <p className="FooterDescription"> contacto@transcurrin.cl</p>
+          </article>
+
+          <article className="FooterButtonContact">
+            <NavLink to={"/categoria/CONTACTO"} className="FooterLinkContact"> OBTEN TU COTIZACIÓN HOY </NavLink>
+          </article>
+          <p className="FooterButtonMensaje"> ¡ Solo válido para días hábiles !</p>
+
+
+        </section>
+    </footer>
   );
 }
 
--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -63,28 +63,28 @@ function HomePage() {
           <section className='StaticsCounters'>
             <article className='Counter'>
               <CountUp start={0} end={1500} duration={10} separator="," className='CounterItem' formattingFn={(value) => `+${value}`} />
-              <p className='titleCardsSection1'>SERVICIOS REALIZADOS</p>
-              <p className='descriptionCardsSection1'>En todas sus modalidades, servicio de unidades full, desconsolidados, consolidados, etc.</p>
+              <p className='CounterTitle'>SERVICIOS REALIZADOS</p>
+              <p className='CounterDescription'>En todas sus modalidades, servicio de unidades full, desconsolidados, consolidados, etc.</p>
             </article>
 
             <article className='Counter'>
               <CountUp start={0} end={25} duration={10} separator="," className='CounterItem' formattingFn={(value) => `+${value}`} />
-              <p className='titleCardsSection1'>CAMIONES EN NUESTRA FLOTA</p>
-              <p className='descriptionCardsSection1'>Durante 2022 y a pesar de una baja considerable en el movimiento de carga respecto a 2021, mantuvimos una flota de +25 camiones disponibles para nuestros clientes.</p>
+              <p className='CounterTitle'>CAMIONES EN NUESTRA FLOTA</p>
+              <p className='CounterDescription'>Durante 2022 y a pesar de una baja considerable en el movimiento de carga respecto a 2021, mantuvimos una flota de +25 camiones disponibles para nuestros clientes.</p>
 
             </article>
 
             <article className='Counter'>
               <CountUp start={0} end={90} duration={10} separator="," className='CounterItem' formattingFn={(value) => `${value}%`} />
-              <p className='titleCardsSection1'> PUNTUALIDAD PRESENTACION</p>
-              <p className='descriptionCardsSection1'>Logramos presentarnos al menos 15 minutos antes del horario de presentación, en más del 90% de nuestros servicios en 2022</p>
+              <p className='CounterTitle'> PUNTUALIDAD PRESENTACION</p>
+              <p className='CounterDescription'>Logramos presentarnos al menos 15 minutos antes del horario de presentación, en más del 90% de nuestros servicios en 2022</p>
 
             </article>
 
             <article className='Counter'>
               <CountUp start={0} end={35} duration={10} separator="," className='CounterItem' formattingFn={(value) => `+${value}`} />
-              <p className='titleCardsSection1'>AÑOS DE EXPERIENCIA</p>
-              <p className='descriptionCardsSection1'>En 2022 cumplimos 35 años conectando el puerto de san antonio con las distintas empresas del pais</p>
+              <p className='CounterTitle'>AÑOS DE EXPERIENCIA</p>
+              <p className='CounterDescription'>En 2022 cumplimos 35 años conectando el puerto de san antonio con las distintas empresas del pais</p>
 
             </article>
 
